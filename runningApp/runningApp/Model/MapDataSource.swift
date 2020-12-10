c3ffd43076794bac11b2ebab20fd53b29f95615e
Massimiliano <massimiliano.bonafede@altran.it>
2020-12-11 00:16:06 +0100

    code refactor

--- runningApp/runningApp/Model/MapDataSource.swift
+++ runningApp/runningApp/Model/MapDataSource.swift
@@ -23,22 +23,22 @@ protocol MapDataSourceProtocol: class {
 class MapDataSource: NSObject, CLLocationManagerDelegate, MKMapViewDelegate {
     
     
-     var delegate: MapDataSourceProtocol?
-     var map: MKMapView
-     let locationManager = CLLocationManager()
-     let regionMeter : Double = 1000
-     var polylineLocation = [CLLocationCoordinate2D]()
-     var arrayGeo = [GeoPoint?]()
-     var startLocation : CLLocation!
-     var latitudine : Double = 0.0
-     var longitude : Double = 0.0
-     var runDistance = 0.0
-     var lastLocation : CLLocation!
-     var counter = 0
-     var arrayKM = [Double]()
-     var speedMax : Double = 0.0
-     var isEndRun: Bool = true
-     var username: String
+    var delegate: MapDataSourceProtocol?
+    var map: MKMapView
+    let locationManager = CLLocationManager()
+    let regionMeter : Double = 1000
+    var polylineLocation = [CLLocationCoordinate2D]()
+    var arrayGeo = [GeoPoint?]()
+    var startLocation : CLLocation!
+    var latitudine : Double = 0.0
+    var longitude : Double = 0.0
+    var runDistance = 0.0
+    var lastLocation : CLLocation!
+    var counter = 0
+    var arrayKM = [Double]()
+    var speedMax : Double = 0.0
+    var isEndRun: Bool = true
+    var username: String
     
     var getCoordinateCLLocationCoordinate2D: (Double, Double) -> (CLLocationCoordinate2D) = {lat, long in
         CLLocationCoordinate2D(latitude: lat, longitude: long)
@@ -68,13 +68,14 @@ class MapDataSource: NSObject, CLLocationManagerDelegate, MKMapViewDelegate {
         checkLocationServices()
     }
     
-    func checkLocationServices(){
-        if CLLocationManager.locationServicesEnabled(){
-            setupLocationManager()
-            checkLocationAuthorizzation()
-        } else {
+    func checkLocationServices() {
+        guard CLLocationManager.locationServicesEnabled() else {
             #warning("show alert letting user know they have to turn this on")
+            return
         }
+        
+        setupLocationManager()
+        checkLocationAuthorizzation()
     }
     
     func setupLocationManager() {
@@ -109,10 +110,10 @@ class MapDataSource: NSObject, CLLocationManagerDelegate, MKMapViewDelegate {
     }
     
     func centerViewOnUserLocation() {
-        if let location = locationManager.location?.coordinate {
-            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionMeter, longitudinalMeters: regionMeter)
-            map.setRegion(region, animated: true)
-        }
+        guard let location = locationManager.location?.coordinate else { return }
+        
+        let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionMeter, longitudinalMeters: regionMeter)
+        map.setRegion(region, animated: true)
     }
     
     
@@ -125,41 +126,37 @@ class MapDataSource: NSObject, CLLocationManagerDelegate, MKMapViewDelegate {
         let region = MKCoordinateRegion(center: center, latitudinalMeters: regionMeter, longitudinalMeters: regionMeter)
         map.setRegion(region, animated: true)
         
+        guard isEndRun else { return }
         
-   
-
-        if isEndRun {
+        
+        latitudine = locationFirst.coordinate.latitude
+        longitude = locationFirst.coordinate.longitude
+        
+        let inizio = getCoordinateCLLocationCoordinate2D(locationFirst.coordinate.latitude,locationFirst.coordinate.longitude)
+        //let inizio = CLLocationCoordinate2D(latitude: locationFirst.coordinate.latitude, longitude: locationFirst.coordinate.longitude)
+        let fine = getCoordinateCLLocationCoordinate2D(location.coordinate.latitude, location.coordinate.latitude)
+        //let fine = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.latitude)
+        
+        drawLine(inizio,fine)
+        
+        if startLocation == nil {
+            startLocation = locations.first
+        } else if let locations = locations.last {
+            runDistance += lastLocation.distance(from: locations)
             
-            latitudine = locationFirst.coordinate.latitude
-            longitude = locationFirst.coordinate.longitude
-
-            let inizio = getCoordinateCLLocationCoordinate2D(locationFirst.coordinate.latitude,locationFirst.coordinate.longitude)
-            //let inizio = CLLocationCoordinate2D(latitude: locationFirst.coordinate.latitude, longitude: locationFirst.coordinate.longitude)
-            let fine = getCoordinateCLLocationCoordinate2D(location.coordinate.latitude, location.coordinate.latitude)
-            //let fine = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.latitude)
+            delegate?.addTotalKm(km: "\(runDistance.metersToMiles(places: 3))")
             
-            drawLine(inizio,fine)
+            guard counter > 0, runDistance > 0 else { return }
             
-            if startLocation == nil {
-                startLocation = locations.first
-            } else if let locations = locations.last {
-                runDistance += lastLocation.distance(from: locations)
-                
-                delegate?.addTotalKm(km: "\(runDistance.metersToMiles(places: 3))")
-                
-                if counter > 0 && runDistance > 0 {
-                    arrayKM.append(lastLocation.speed)
-                    delegate?.addAvarageSpeed(speed: "\((lastLocation.speed * 3.6).twoDecimalNumbers(place: 1)) Km/h")
-                    
-                    speedMax = calcolaMedia(arrayKM)
-                }
-            }
-            lastLocation = locations.last
-        } else {
-            // check this function
+            arrayKM.append(lastLocation.speed)
+            delegate?.addAvarageSpeed(speed: "\((lastLocation.speed * 3.6).twoDecimalNumbers(place: 1)) Km/h")
+            
+            speedMax = calcolaMedia(arrayKM)
             
         }
+        lastLocation = locations.last
     }
+    
     
     
     
@@ -183,27 +180,22 @@ class MapDataSource: NSObject, CLLocationManagerDelegate, MKMapViewDelegate {
     }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        if annotation is MKUserLocation {
-            let pin = mapView.view(for: annotation) as? MKPinAnnotationView ?? MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
-            pin.pinTintColor = #colorLiteral(red: 0.2745098039, green: 0.5490196078, blue: 0.9019607843, alpha: 1)
-            let label = UILabel()
-            label.text = username
-            label.textAlignment = .center
-            pin.addSubview(label)
-            label.translatesAutoresizingMaskIntoConstraints = false
-            label.bottomAnchor.constraint(equalTo: pin.topAnchor).isActive = true
-            label.centerXAnchor.constraint(equalTo: pin.centerXAnchor).isActive = true
-            return pin
-
-        } else {
-            // handle other annotations
-
-        }
-        return nil
+        guard annotation is MKUserLocation else { return nil }
+        
+        let pin = mapView.view(for: annotation) as? MKPinAnnotationView ?? MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        pin.pinTintColor = #colorLiteral(red: 0.2745098039, green: 0.5490196078, blue: 0.9019607843, alpha: 1)
+        let label = UILabel()
+        label.text = username
+        label.textAlignment = .center
+        pin.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.bottomAnchor.constraint(equalTo: pin.topAnchor).isActive = true
+        label.centerXAnchor.constraint(equalTo: pin.centerXAnchor).isActive = true
+        return pin
     }
-
     
-
+    
+    
     func drawLine(_ startCoordinate : CLLocationCoordinate2D, _ endingRun : CLLocationCoordinate2D) {
         polylineLocation.append(startCoordinate)
         arrayGeo.append(GeoPoint(latitude: startCoordinate.latitude, longitude: startCoordinate.longitude))
@@ -211,8 +203,6 @@ class MapDataSource: NSObject, CLLocationManagerDelegate, MKMapViewDelegate {
         self.map.addOverlay(aPolyline)
         
     }
-    
-
     
 }
 
